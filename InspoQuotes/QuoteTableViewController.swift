d7149465a4ee89b50a610fc43f62ccbf85ee451b
MohamedJaber <33813295+MohamedJaber@users.noreply.github.com>
2020-12-29 12:37:23 +0200

    Inspiration App

---
+++ InspoQuotes/QuoteTableViewController.swift
@@ -0,0 +1,122 @@
+//
+//  QuoteTableViewController.swift
+//  InspoQuotes
+//
+//  Created by Angela Yu on 18/08/2018.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import StoreKit
+
+class QuoteTableViewController: UITableViewController, SKPaymentTransactionObserver {
+    
+    let productID=""
+    var quotesToShow = [
+        "Our greatest glory is not in never falling, but in rising every time we fall. — Confucius",
+        "All our dreams can come true, if we have the courage to pursue them. – Walt Disney",
+        "It does not matter how slowly you go as long as you do not stop. – Confucius",
+        "Everything you’ve ever wanted is on the other side of fear. — George Addair",
+        "Success is not final, failure is not fatal: it is the courage to continue that counts. – Winston Churchill",
+        "Hardships often prepare ordinary people for an extraordinary destiny. – C.S. Lewis"
+    ]
+    
+    let premiumQuotes = [
+        "Believe in yourself. You are braver than you think, more talented than you know, and capable of more than you imagine. ― Roy T. Bennett",
+        "I learned that courage was not the absence of fear, but the triumph over it. The brave man is not he who does not feel afraid, but he who conquers that fear. – Nelson Mandela",
+        "There is only one thing that makes a dream impossible to achieve: the fear of failure. ― Paulo Coelho",
+        "It’s not whether you get knocked down. It’s whether you get up. – Vince Lombardi",
+        "Your true success in life begins only when you make the commitment to become excellent at what you do. — Brian Tracy",
+        "Believe in yourself, take on your challenges, dig deep within yourself to conquer fears. Never let anyone bring you down. You got to keep going. – Chantal Sutherland"
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        SKPaymentQueue.default().add(self)
+        if isPruchased(){
+            showPremiumQuotes()
+        }
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        if isPruchased(){
+            return quotesToShow.count}
+        else{
+            return quotesToShow.count
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "QuoteCell", for: indexPath)
+        if indexPath.row<quotesToShow.count{
+            cell.textLabel?.text=quotesToShow[indexPath.row]
+            cell.textLabel?.numberOfLines=0// to make it big as you want.
+            cell.textLabel?.textColor=#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+            cell.accessoryType = .none
+        }
+        else{
+            cell.textLabel?.text="get more quotes"
+            cell.textLabel?.textColor=#colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+            cell.accessoryType = .disclosureIndicator
+            }
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row==quotesToShow.count{
+            buyPremiumQuotes()
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    func buyPremiumQuotes(){
+        if SKPaymentQueue.canMakePayments(){
+            //Can make payments
+            let paymentRequest=SKMutablePayment()
+            paymentRequest.productIdentifier = productID
+            SKPaymentQueue.default().add(paymentRequest)
+        }
+        else {
+            //Can't make payments
+        }
+    }
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        for transaction in transactions{
+            if transaction.transactionState == .purchased{
+                //User did buy
+                showPremiumQuotes()
+                UserDefaults.standard.set(true, forKey: productID)
+                SKPaymentQueue.default().finishTransaction(transaction)
+            }else if transaction.transactionState == .failed {
+                //User failed to buy
+                if let error=transaction.error{
+                    let errorDescription=error.localizedDescription
+                    print("Transaction failed due to an error \(errorDescription)")
+                }
+                SKPaymentQueue.default().finishTransaction(transaction)
+            }else if transaction.transactionState == .restored {
+                showPremiumQuotes()
+                navigationItem.setRightBarButton(nil, animated: true)
+                SKPaymentQueue.default().finishTransaction(transaction)
+            }
+        }
+    }
+    @IBAction func restorePressed(_ sender: UIBarButtonItem) {
+        SKPaymentQueue.default().restoreCompletedTransactions()
+    }
+    func showPremiumQuotes(){
+        UserDefaults.standard.set(true, forKey: productID)
+        quotesToShow.append(contentsOf: premiumQuotes)
+        tableView.reloadData()
+    }
+    func isPruchased()->Bool{
+        let purchaseStatus=UserDefaults.standard.bool(forKey: productID)
+        if purchaseStatus{
+            return true
+        }else{
+            return false
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+
+# InspoQuotes
+
+
+
+
+## InspoQuotes is a freemium app that gives some free inspiration and a lot of paid inspiration.  
+
+
+* Uses the StoreKit API to allow users to purchase premium content
+* Uses the StoreKit API to detect failure cases
+* Uses the StoreKit API to restore in-app purchases
+* Persists the state of users' purchases
+
